59750aca3d22c7f5b73bafce93296276e1eb5812
Sergio Moreno <sergiomoreno.tic@ucaldas.edu.co>
2021-10-02 19:16:13 -0500

    Proyecto servicios

--- Ganaderia/Ganaderia.App/Ganaderia.App.Servicios/Controllers/WeatherForecastController.cs
+++ Ganaderia/Ganaderia.App/Ganaderia.App.Servicios/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Ganaderia.App.Dominio;
 
 namespace Ganaderia.App.Servicios.Controllers
 {
@@ -16,6 +17,8 @@ namespace Ganaderia.App.Servicios.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        public List<Ganadero> Ganaderos { get; set; }
+
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -24,16 +27,84 @@ namespace Ganaderia.App.Servicios.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<Ganadero> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            // var rng = new Random();
+            // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            // {
+            //     Date = DateTime.Now.AddDays(index),
+            //     TemperatureC = rng.Next(-20, 55),
+            //     Summary = Summaries[rng.Next(Summaries.Length)]
+            // })
+            // .ToArray();
+
+            var ganadero = new Ganadero
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                Nombres = "Sergio",
+                Apellidos = "Moreno",
+                NumeroTelefono = "3148596562",
+                Correo = "sergiomintic@mintic.edu.co",
+                Contrasena = "123456",
+                Latitud = 4554,
+                Longitud = 5454
+            };
+
+            var ganadero2 = new Ganadero
+            {
+                Nombres = "Cristiano",
+                Apellidos = "Ronaldo",
+                NumeroTelefono = "3148596562",
+                Correo = "Ronaldo@mintic.edu.co",
+                Contrasena = "123456",
+                Latitud = 4554,
+                Longitud = 5454
+            };
+
+            var ganadero3 = new Ganadero
+            {
+                Nombres = "Sergio",
+                Apellidos = "Aguero",
+                NumeroTelefono = "3148596562",
+                Correo = "Sergio@mintic.edu.co",
+                Contrasena = "123456",
+                Latitud = 4554,
+                Longitud = 5454
+            };
+
+            var ganadero4 = new Ganadero
+            {
+                Nombres = "Juan",
+                Apellidos = "Cuadrado",
+                NumeroTelefono = "3148596562",
+                Correo = "Juan@mintic.edu.co",
+                Contrasena = "123456",
+                Latitud = 4554,
+                Longitud = 5454
+            };
+
+            var ganadero5 = new Ganadero
+            {
+                Nombres = "David",
+                Apellidos = "Ospina",
+                NumeroTelefono = "3148596562",
+                Correo = "sergiomintic@mintic.edu.co",
+                Contrasena = "123456",
+                Latitud = 4554,
+                Longitud = 5454
+            };
+
+
+            Ganaderos = new List<Ganadero>();
+
+            Ganaderos.Add(ganadero);
+            Ganaderos.Add(ganadero2);
+            Ganaderos.Add(ganadero3);
+            Ganaderos.Add(ganadero4);
+            Ganaderos.Add(ganadero5);
+
+            return Ganaderos;
+
+
         }
     }
 }
